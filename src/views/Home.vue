3e977b6f37ca9e14965e3691ba01d2e443260b3b
nilsreich <nilsreich@gmail.com>
2021-09-27 17:47:09 +0200

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,27 +3,36 @@ import ButtonRepo from '@/components/ButtonRepo.vue'
 </script>
 
 <template>
-  <div class="bg-gray-50">
-    <div
-      class="max-w-screen-xl px-4 py-12 mx-auto sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between"
-    >
-      <h2
-        class="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 sm:text-4xl sm:leading-10"
-      >
-        Ready to dive in?
-        <br />
-        <span class="text-indigo-600">Vite + Vue 3 + Tailwind CSS</span>
-      </h2>
-      <div class="flex mt-8 lg:flex-shrink-0 lg:mt-0">
-        <div class="inline-flex rounded-md shadow">
-          <router-link
-            to="/about"
-            class="inline-flex items-center justify-center px-5 py-3 text-base font-medium leading-6 text-white transition duration-150 ease-in-out bg-indigo-600 border border-transparent rounded-md hover:bg-indigo-500 focus:outline-none"
-            >Next Page</router-link
-          >
-        </div>
-        <ButtonRepo />
+      <div class="flex border-b bg-white pl-10 py-2 text-gray-500 justify-between fixed w-screen">
+      <div class="font-semibold p-2 ">Home</div>
+      <div  class="flex mr-10">
+      <div><svg class="w-4 h-4 fill-current m-3" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
+  <path d="M15 3a1 1 0 0 0-.2 0 13.1 13.1 0 0 0 1.8 26h.5a13 13 0 0 0 10.7-5.6A1 1 0 0 0 27 22 13 13 0 0 1 16 4.4 1 1 0 0 0 15 3Z"/>
+  <path d="M0 0h32v32H0z" data-name="&lt;Transparent Rectangle&gt;" style="fill:none"/>
+</svg>
       </div>
-    </div>
-  </div>
+      <div class="block md:hidden"><svg class="w-5 h-5 fill-current m-3" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
+  <path d="M4 6h24v2H4zm0 18h24v2H4zm0-12h24v2H4zm0 6h24v2H4z"/>
+  <path d="M0 0h32v32H0z" data-name="&lt;Transparent Rectangle&gt;" style="fill:none"/>
+</svg></div>
+      </div>
+      </div>
+<div class="flex ">
+    <aside class=" hidden md:block h-screen sticky top-0">
+  <div class="pr-10 ml-5 mr-20 w-60 md:1/5 mt-20 text-gray-700 ">
+  <div class="text-green-900 bg-green-100 h-10 px-4 py-3 text-sm rounded">Einleitung</div>
+  <div class=" text-sm h-9 px-4 py-3 text-gray-500 hover:text-green-900 ">Wie bilde ich mir eine Meinung in der digitalen Welt?</div>
+  </aside>
+  <main class="mx-10 mt-10 lg:mx-0">
+
+  <div class="prose prose-sm sm:prose lg:prose-lg xl:prose-lg">
+
+    <article>
+      <h2>Einleitung</h2>
+      <img src="https://edv.vercel.app/abb12.png" />
+     <p class="">Die Nachrichten- und Informationswelt hat sich in den letzten 20 Jahren gravierend verändert. Hätte in der Zeit vor den sozialen Medien ein Politiker wie Donald Trump jederzeit unbegrenzt und massenhaft Unwahrheiten absetzen können und damit Gehör bei seinem Publikum gefunden? Wären Corona-Verschwörungsmythen und Desinformationen wie in der „Infodemie“1 vielfach verbreitet und geteilt worden? Wohl eher nicht. Auch wenn früher nicht alles besser war, so lassen sich doch seit einiger Zeit Entwicklungen im Mediensystem beobachten, die eine Verständigung über Fakten und einen Zusammenhalt der Gesellschaft erschweren. Diese Herausforderung bringt Barack Obama mit seiner Äußerung auf den Punkt: „Vielleicht am schädlichsten ist die Tatsache, dass jede Seite inzwischen ihre eigenen Medien und folglich ihre eigenen Fakten hat. Es ist sehr schwierig, Kompromisse zu erzielen, wenn man nicht von einer gemeinsamen Realität ausgeht.“ 2 Auch wenn das US-amerikanische Mediensystem nicht mit unserem vergleichbar ist, so ist doch auch hier bei den Nutzerinnen und Nutzern eine zunehmende Verunsicherung über den Wahrheitsgehalt von Informationen und eine teilweise Überforderung bei der Aufdeckung von Desinformation zu erkennen. Dies gilt übrigens nicht nur für Jugendliche. Jugendliche, die in diesem digitalen Kosmos aufwachsen, sollten jedoch dabei unterstützt werden, ihre Informationsund Meinungsbildungskompetenz auszubilden. Denn diese Fähigkeit ist die DNA für ein selbstbestimmtes Leben in einer Gesellschaft, die auf Freiheit, Verständigung, Wahrhaftigkeit und das Aushandeln politischer, sozialer und wirtschaftlicher Interessen beruht. Aus medienethischer Sicht bedarf eine moderne liberale Demokratie nicht nur informationsmündiger Bürgerinnen und Bürger. Es braucht auch einen Konsens darüber, dass das Streben nach Wahrheit ein unentbehrliches Gut ist und die Anerkennung der Wissenschaft als Referenzsystem für Fakten unabdingbar ist. Wie kann man Jugendliche dabei unterstützen, sich kompetent zu informieren und eine fundierte Meinung zu bilden? Das vorliegende Arbeitsbuch nutzt hierfür die Medienethische Roadmap zur Meinungsbildungskompetenz.</p>
+    </article>
+  </main>
+</div>
+
 </template>
